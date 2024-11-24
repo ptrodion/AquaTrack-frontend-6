19f919fd04430540591b3f542c5adad9035c8d62
Kateryna-Kupriianova <kupriyanov.forever@gmail.com>
2024-11-25 00:55:28 +0200

    mobile done

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-// import 'assets/styles/styles.css';
+import './assets/styles/reset.css';
+
 
 // import { Provider } from 'react-redux';
 // import { persistor, store } from './redux/store.js';
