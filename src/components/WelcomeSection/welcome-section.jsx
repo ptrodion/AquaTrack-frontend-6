bfee5f08cd5f1df01d71600a87f38bcbe4967bb5
Kateryna-Kupriianova <kupriyanov.forever@gmail.com>
2024-11-24 14:06:16 +0200

    add HomePage markup

--- src/components/WelcomeSection/welcome-section.jsx
+++ src/components/WelcomeSection/welcome-section.jsx
@@ -6,6 +6,23 @@ const WelcomeSection = () => {
     <section className={styles.section}>
       <div className={styles.container}>
         <Logo />
+        <div className={styles.textWrapper}>
+
+          <h3 className={styles.subtitle}>Record daily water intake and track</h3>
+
+          <h1 className={styles.title}>Water consumption tracker</h1>
+
+        </div>
+        <div className={styles.links}>
+          <button>
+            <a href="#" className={styles.tryTracker}>Try tracker</a>
+
+          </button>
+          <button>
+            <a href="#" className={styles.signIn}>Sign in</a>
+          </button>
+
+        </div>
       </div>
     </section>
   );
