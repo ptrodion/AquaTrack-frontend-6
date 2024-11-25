34c251448220c31564e62071587feb050a604fa9
Eduard <edik.moskalets@gmail.com>
2024-11-25 22:24:48 +0200

    UserPanel(UserBar)

--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -1,5 +1,13 @@
+import UserBar from "../../components/UserBar/UserBar.jsx"
+
 function TrackerPage() {
-  return <h1>TrackerPage</h1>;
+  return (
+    <div><UserBar
+    name="Nadia"
+    avatarUrl="https://example.com/avatar.jpg"/>
+    <h1>TrackerPage</h1></div>
+  
+  );
 }
 
 export default TrackerPage;
