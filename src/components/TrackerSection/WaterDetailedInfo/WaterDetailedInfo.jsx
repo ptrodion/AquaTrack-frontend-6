4fdbad7f21953193317f74b760361c01136df78b
Eduard <edik.moskalets@gmail.com>
2024-11-27 19:55:38 +0200

    WaterDetailedInfo

---
+++ src/components/TrackerSection/WaterDetailedInfo/WaterDetailedInfo.jsx
@@ -0,0 +1,14 @@
+import css from './WaterDetailedInfo.module.css';
+
+
+
+const WaterDetailedInfo = () => {
+  return (
+      <div className={css.waterDetailedInfo}>
+          WaterDetailedInfo
+      
+      </div>
+  )
+}
+
+export default WaterDetailedInfo
