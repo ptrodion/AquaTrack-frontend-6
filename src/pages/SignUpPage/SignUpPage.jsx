2cc79e29d0042a39d07b828197e51e04f4ed7062
b1mbula <danja.rubeghanskij@gmail.com>
2024-11-27 11:43:42 +0100

    SignUpPage(Desktop version)

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,10 +1,15 @@
+import AdvantagesSection from 'components/AdvantagesSection/advantages-section';
 import SignUpForm from 'components/SignUpForm/SignUpForm';
+import css from './SignUpPage.module.css';
 
 function SignUpPage() {
   return (
-    <>
+    <div className={css.signUpContainer}>
       <SignUpForm></SignUpForm>
-    </>
+      <div className={css.advantageContainer}>
+        <AdvantagesSection></AdvantagesSection>
+      </div>
+    </div>
   );
 }
 
