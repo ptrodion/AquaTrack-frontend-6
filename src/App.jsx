f7b261089df14cd269e7ddec8f316491ab06dfaa
Ruslan-Chyzhenko <144059463+Ruslan-Chyzhenko@users.noreply.github.com>
2024-11-29 01:22:12 +0100

    start

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import SharedLayout from 'components/SharedLayout/SharedLayout.jsx';
 import { lazy } from 'react';
 import { Toaster } from 'react-hot-toast';
 import { Route, Routes } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 
 const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
 const SignInPage = lazy(() => import('./pages/SignInPage/SignInPage'));
 const SignUpPage = lazy(() => import('./pages/SignUpPage/SignUpPage'));
 const TrackerPage = lazy(() => import('./pages/TrackerPage/TrackerPage'));
+
+const TestPage = lazy(() => import('./pages/TestPage/TestPage'));
 
 function App() {
   return (
@@ -16,7 +19,16 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/signin" element={<SignInPage />} />
-        <Route path="/tracker" element={<TrackerPage />} />
+        <Route path="/test" element={<TestPage />} />
+        {/* <Route path="/tracker" element={<TrackerPage />} /> */}
+        <Route
+          path="/tracker"
+          element={
+            <PrivateRoute>
+              <TrackerPage />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </SharedLayout>
   );
--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -1,5 +1,12 @@
+import WaterList from './WaterList';
+
 function TrackerPage() {
-  return <h1>TrackerPage</h1>;
+  // return <h1>TrackerPage</h1>;
+  return (
+    <div className="tracker-page">
+      <WaterList />
+    </div>
+  );
 }
 
 export default TrackerPage;
