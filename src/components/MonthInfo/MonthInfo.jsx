e3c054ee89123892ea81ae2c0d2533a7ea775d3a
Denys <Melnik.Denys99@gmail.com>
2024-11-29 22:45:57 +0200

    statistics added

--- src/components/MonthInfo/MonthInfo.jsx
+++ src/components/MonthInfo/MonthInfo.jsx
@@ -1,15 +1,31 @@
 import { Calendar } from 'components/Calendar/Calendar';
 import { CalendarPagination } from 'components/CalendarPagination/CalendarPagination';
+import WaterConsumptionChart from 'components/WaterConsumptionChart/WaterConsumptionChart';
 import css from './MonthInfo.module.css';
+import { useState } from 'react';
+import { FiPieChart } from "react-icons/fi";
 
 const MonthInfo = () => {
+  const [view, setView] = useState('calendar');
+
+  const toggleView = () => {
+    setView((prev) => (prev === 'calendar' ? 'chart' : 'calendar'));
+  };
+
   return (
     <div className={css.calendarWrap}>
       <div className={css.titleWrap}>
-        <h2 className={css.title}>Month</h2>
-        <CalendarPagination />
+        <h2 className={css.title}>{view === 'calendar' ? 'Month' : 'Statistics'}</h2>
+        <div className={css.paginationWrap}>
+          <CalendarPagination />
+          <button onClick={toggleView} className={css.statsToggleBtn}>
+            <FiPieChart size={24} className={css.statsToggleIcon} />
+          </button>
+        </div>
       </div>
-      <Calendar />
+      <div>
+        {view === 'calendar' ? <Calendar /> : <WaterConsumptionChart />}
+      </div>
     </div>
   );
 };
---
+++ src/components/WaterConsumptionChart/WaterConsumptionChart.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+import css from './WaterConsumptionChart';
+
+const getCurrentWeekDates = () => {
+  const currentDate = new Date();
+  const currentDay = currentDate.getDay();
+
+  const startOfWeek = new Date(currentDate);
+  startOfWeek.setDate(currentDate.getDate() - currentDay + 1);
+
+  const weekDates = [];
+  for (let i = 0; i < 7; i++) {
+    const day = new Date(startOfWeek);
+    day.setDate(startOfWeek.getDate() + i);
+    weekDates.push(day);
+  }
+
+  return weekDates;
+};
+
+const WaterConsumptionChart = () => {
+  const [chartData, setChartData] = useState([]);
+
+  useEffect(() => {
+    const weekDates = getCurrentWeekDates();
+    const consumptionData = weekDates.map((date, index) => {
+
+      const dayString = date.getDate();
+
+      const consumption = Math.floor(Math.random() * 3);
+
+      return {
+        day: dayString,
+        consumption: consumption,
+      };
+    });
+
+    setChartData(consumptionData);
+  }, []);
+
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <LineChart data={chartData}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="day"/>
+        <YAxis
+        ticks={[0, 0.5, 1, 1.5, 2, 2.5]}
+        domain={[0, 2.5]}
+        tickFormatter={(tick) => (tick === 0 ? "0" : `${tick}L`)}
+        />
+        <Tooltip />
+        <Line
+          type="monotone"
+          dataKey="consumption"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+        />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default WaterConsumptionChart;
