097cc57bafc3a072f4a77750e82ff7b888786e50
Natalia Huliaieva <155992841+Natatulichka@users.noreply.github.com>
2024-11-29 18:43:15 +0100

    translate

--- src/components/MonthInfo/MonthInfo.jsx
+++ src/components/MonthInfo/MonthInfo.jsx
@@ -1,12 +1,14 @@
 import { Calendar } from 'components/Calendar/Calendar';
 import { CalendarPagination } from 'components/CalendarPagination/CalendarPagination';
 import css from './MonthInfo.module.css';
+import { useTranslation } from 'react-i18next';
 
 const MonthInfo = () => {
+  const { t } = useTranslation();
   return (
     <div className={css.calendarWrap}>
       <div className={css.titleWrap}>
-        <h2 className={css.title}>Month</h2>
+        <h2 className={css.title}>{t('calendar.month')}</h2>
         <CalendarPagination />
       </div>
       <Calendar />
