1aeb4d9fc1fbf3069897228af21d5a6cf3a6fd49
Denys <Melnik.Denys99@gmail.com>
2024-11-30 14:47:41 +0200

    updated

--- src/components/WaterConsumptionChart/WaterConsumptionChart.jsx
+++ src/components/WaterConsumptionChart/WaterConsumptionChart.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React from "react";
 import {
   LineChart,
   Line,
@@ -7,55 +7,29 @@ import {
   CartesianGrid,
   Tooltip,
   ResponsiveContainer,
-} from 'recharts';
+} from "recharts";
 
-const getCurrentWeekDates = () => {
-  const currentDate = new Date();
-  const currentDay = currentDate.getDay();
-
-  const startOfWeek = new Date(currentDate);
-  startOfWeek.setDate(currentDate.getDate() - currentDay + 1);
-
-  const weekDates = [];
-  for (let i = 0; i < 7; i++) {
-    const day = new Date(startOfWeek);
-    day.setDate(startOfWeek.getDate() + i);
-    weekDates.push(day);
-  }
-
-  return weekDates;
-};
-
-const WaterConsumptionChart = () => {
-  const [chartData, setChartData] = useState([]);
-
-  useEffect(() => {
-    const weekDates = getCurrentWeekDates();
-    const consumptionData = weekDates.map(date => {
-      const dayString = date.getDate();
-
-      const consumption = Math.floor(Math.random() * 3);
-
-      return {
-        day: dayString,
-        consumption: consumption,
-      };
-    });
-
-    setChartData(consumptionData);
-  }, []);
-
+const WaterConsumptionChart = ({ weekData }) => {
   return (
     <ResponsiveContainer width="100%" height={300}>
-      <LineChart data={chartData}>
+      <LineChart data={weekData}>
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="day" />
+        <XAxis
+          dataKey={(entry) => entry.day.getDate()}
+          tickFormatter={(tick) => `${tick}`}
+        />
         <YAxis
           ticks={[0, 0.5, 1, 1.5, 2, 2.5]}
           domain={[0, 2.5]}
-          tickFormatter={tick => (tick === 0 ? '0' : `${tick}L`)}
+          tickFormatter={(tick) => (tick === 0 ? "0" : `${tick}L`)}
         />
-        <Tooltip />
+        <Tooltip
+          labelFormatter={(label, payload) => {
+            const date = payload?.[0]?.payload?.day;
+            return date ? date.toDateString() : "Unknown Date";
+          }}
+          formatter={(value) => [`${value.toFixed(1)} L`, "Consumption"]}
+        />
         <Line
           type="monotone"
           dataKey="consumption"
