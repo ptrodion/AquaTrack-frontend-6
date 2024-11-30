6484cb7d565dac31abdd5a2e2e9404b56d751752
Natalia Huliaieva <155992841+Natatulichka@users.noreply.github.com>
2024-11-30 21:50:52 +0100

    sening+signin-Language

--- src/components/UserSettingsForm/UserSettingsForm.jsx
+++ src/components/UserSettingsForm/UserSettingsForm.jsx
@@ -19,14 +19,16 @@ export const UserSettingsForm = ({ onModalClose }) => {
   const [error, setError] = useState(null);
 
   const validationSchema = Yup.object().shape({
-    gender: Yup.string().required('Gender is required'),
+    gender: yup.string().required(t('settingsForm.ValidationGender')),
     name: Yup.string(),
-    email: Yup.string().email('Invalid email').required('Email is required'),
-    weight: Yup.number().positive('Weight must be positive'),
-    activeTime: Yup.number().min(0, 'Active time cannot be negative'),
+    email: Yup.string()
+      .email(t('settingsForm.ValidationEmail'))
+      .required(t('settingsForm.ValidationEmailRequired')),
+    weight: Yup.number().positive(t('settingsForm.ValidationWeightPositiv')),
+    activeTime: Yup.number().min(0, t('settingsForm.ValidationTimeTypeError')),
     dailyWaterIntake: Yup.number()
-      .positive('Daily water intake must be positive')
-      .required('Daily water intake is required'),
+      .positive(t('settingsForm.ValidationDailyRequirementMin'))
+      .required(t('settingsForm.ValidationDailyRequirementMin')),
   });
 
   const {
@@ -39,13 +41,10 @@ export const UserSettingsForm = ({ onModalClose }) => {
     defaultValues: json,
   });
 
-
   const onSubmit = async data => {
-
     const formData = new FormData();
 
-      console.log("data", data);
-
+    console.log('data', data);
 
     formData.append('gender', data.gender);
     formData.append('name', data.name);
@@ -55,11 +54,10 @@ export const UserSettingsForm = ({ onModalClose }) => {
     formData.append('waterIntake', data.waterIntake);
     formData.append('avatarUrl', avatar);
     console.log('formData', formData);
-    onModalClose()
+    onModalClose();
     // try {
     //   console.log('formData', formData);
     //   console.log("data", data);
-
 
     //   // const response = await axios.post('/api/user/update', formData);
     //   // onUpdate(response.data);
@@ -75,150 +73,150 @@ export const UserSettingsForm = ({ onModalClose }) => {
   return (
     <>
       {/* {error && ( */}
-        <form className="user-settings-form" onSubmit={onSubmit}>
-          <div className={css.formGroup}>
-            {!avatar && (
-              <img
-                src={initialAvatar}
-                alt="Avatar Preview"
-                className={css.image}
-              />
-            )}
-            {avatar && (
-              <img
-                src={URL.createObjectURL(avatar)}
-                alt="Avatar Preview"
-                className={css.image}
-              />
-            )}
-            <label name="file-upload" className={css.customFileLabel}>
-              <UploadOutlined /> Upload a photo
+      <form className="user-settings-form" onSubmit={onSubmit}>
+        <div className={css.formGroup}>
+          {!avatar && (
+            <img
+              src={initialAvatar}
+              alt="Avatar Preview"
+              className={css.image}
+            />
+          )}
+          {avatar && (
+            <img
+              src={URL.createObjectURL(avatar)}
+              alt="Avatar Preview"
+              className={css.image}
+            />
+          )}
+          <label name="file-upload" className={css.customFileLabel}>
+            <UploadOutlined /> {t('settingsForm.userUploadButton')}
+            <input
+              name="file-upload"
+              type="file"
+              onChange={handleAvatarChange}
+            />
+          </label>
+        </div>
+
+        <div className={css.genderGroup}>
+          <label className={css.label}>{t('settingsForm.userGender')}:</label>
+          <div className={css.radioBox}>
+            <label>
               <input
-                name="file-upload"
-                type="file"
-                onChange={handleAvatarChange}
+                type="radio"
+                value="Woman"
+                className={css.radioInput}
+                {...register('gender')}
               />
+              {t('settingsForm.genderWoman')}
+            </label>
+            <label>
+              <input
+                type="radio"
+                value="Man"
+                className={css.radioInput}
+                {...register('gender')}
+              />
+              {t('settingsForm.genderMan')}
             </label>
           </div>
+          {errors.gender && <p className="error">{errors.gender.message}</p>}
+        </div>
 
-          <div className={css.genderGroup}>
-            <label className={css.label}>Your gender identity:</label>
-            <div className={css.radioBox}>
-              <label>
-                <input
-                  type="radio"
-                  value="Woman"
-                  className={css.radioInput}
-                  {...register('gender')}
-                />
-                Woman
-              </label>
-              <label>
-                <input
-                  type="radio"
-                  value="Man"
-                  className={css.radioInput}
-                  {...register('gender')}
-                />
-                Man
-              </label>
-            </div>
-            {errors.gender && <p className="error">{errors.gender.message}</p>}
+        <div className={css.flexGroup}>
+          <div className={css.nameGroup}>
+            <label className={css.label}>
+              {t('settingsForm.userNameLabel')}:
+            </label>
+            <input type="text" {...register('name')} className={css.input} />
+            {errors.name && <p className="error">{errors.name.message}</p>}
           </div>
 
-          <div className={css.flexGroup}>
-            <div className={css.nameGroup}>
-              <label className={css.label}>Your name:</label>
-              <input type="text" {...register('name')} className={css.input} />
-              {errors.name && <p className="error">{errors.name.message}</p>}
-            </div>
-
-            <div className={css.emailGroup}>
-              <label className={css.label}>Email:</label>
-              <input
-                type="email"
-                {...register('email')}
-                className={css.input}
-              />
-              {errors.email && <p className="error">{errors.email.message}</p>}
-            </div>
-
-            <div>
-              <p className={css.label}>My daily norma</p>
-              <ul className={css.list}>
-                <li className={css.item}>
-                  <p>For woman:</p>
-                  <p className={css.formula}>V=(M*0,03) + (T*0,4)</p>
-                </li>
-                <li className={css.item}>
-                  <p>For man:</p>
-                  <p className={css.formula}>V=(M*0,04) + (T*0,6)</p>
-                </li>
-              </ul>
-
-              <p className={css.description}>
-                * V is the volume of the water norm in liters per day, M is your
-                body weight, T is the time of active sports, or another type of
-                activity commensurate in terms of loads (in the absence of
-                these, you must set 0)
-              </p>
-              <p className={css.textActive}>
-                <span className={css.formula}>!</span>Active time in hours
-              </p>
-            </div>
-
-            <div className={css.weightGroup}>
-              <label className={css.label}>Your weight (kg):</label>
-              <input
-                type="number"
-                {...register('weight')}
-                className={css.input}
-              />
-              {errors.weight && (
-                <p className="error">{errors.weight.message}</p>
-              )}
-            </div>
-
-            <div className={css.emailGroup}>
-              <label className={css.label}>Active time per day (hours):</label>
-              <input
-                type="number"
-                {...register('activeTime')}
-                className={css.input}
-              />
-              {errors.activeTime && (
-                <p className="error">{errors.activeTime.message}</p>
-              )}
-            </div>
-
-            <div className={css.text}>
-              <p>
-                The required amount of water in liters per day:{' '}
-                <span className={css.span}>1.8 L</span>{' '}
-              </p>
-            </div>
-
-            <div className={css.emailGroup}>
-              <label className={css.label}>
-                Write down how much water you will drink:
-              </label>
-              <input
-                type="number"
-                step="0.1"
-                min="0"
-                {...register('waterIntake')}
-                className={css.input}
-              />
-              {errors.waterIntake && (
-                <p className="error">{errors.waterIntake.message}</p>
-              )}
-            </div>
+          <div className={css.emailGroup}>
+            <label className={css.label}>
+              {' '}
+              {t('settingsForm.userEmailLabel')}:
+            </label>
+            <input type="email" {...register('email')} className={css.input} />
+            {errors.email && <p className="error">{errors.email.message}</p>}
           </div>
 
-          <button type="submit" className={css.buttonSubmit}>
-            Save
-          </button>
-        </form>
+          <div>
+            <p className={css.label}>{t('settingsForm.subtitle')}</p>
+            <ul className={css.list}>
+              <li className={css.item}>
+                <p>{t('settingsForm.textForWoman')}:</p>
+                <p className={css.formula}>V=(M*0,03) + (T*0,4)</p>
+              </li>
+              <li className={css.item}>
+                <p>{t('settingsForm.textForWoman')}:</p>
+                <p className={css.formula}>V=(M*0,04) + (T*0,6)</p>
+              </li>
+            </ul>
+
+            <p className={css.description}>
+              * {t('settingsForm.textDescription"')}
+            </p>
+            <p className={css.textActive}>
+              <span className={css.formula}>!</span>
+              {t('settingsForm.textActiveTime')}
+            </p>
+          </div>
+
+          <div className={css.weightGroup}>
+            <label className={css.label}>
+              {' '}
+              {t('settingsForm.userWeight')}:
+            </label>
+            <input
+              type="number"
+              {...register('weight')}
+              className={css.input}
+            />
+            {errors.weight && <p className="error">{errors.weight.message}</p>}
+          </div>
+
+          <div className={css.emailGroup}>
+            <label className={css.label}>{t('settingsForm.userWeight')}:</label>
+            <input
+              type="number"
+              {...register('activeTime')}
+              className={css.input}
+            />
+            {errors.activeTime && (
+              <p className="error">{errors.activeTime.message}</p>
+            )}
+          </div>
+
+          <div className={css.text}>
+            <p>
+              {t('settingsForm.WaterAmount')}:{' '}
+              <span className={css.span}>1.8 {t('chooseDate.l')}:</span>{' '}
+            </p>
+          </div>
+
+          <div className={css.emailGroup}>
+            <label className={css.label}>
+              {t('settingsForm.labelWaterNorma')}:
+            </label>
+            <input
+              type="number"
+              step="0.1"
+              min="0"
+              {...register('waterIntake')}
+              className={css.input}
+            />
+            {errors.waterIntake && (
+              <p className="error">{errors.waterIntake.message}</p>
+            )}
+          </div>
+        </div>
+
+        <button type="submit" className={css.buttonSubmit}>
+          {t('settingsForm.subButton')}
+        </button>
+      </form>
       {/* )} */}
     </>
   );
