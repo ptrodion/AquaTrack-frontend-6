d021640222b019eb6b9162bee0b822a996b4f648
Natalia Huliaieva <155992841+Natatulichka@users.noreply.github.com>
2024-12-01 17:45:17 +0100

    translanted month

--- src/components/CalendarPagination/CalendarPagination.jsx
+++ src/components/CalendarPagination/CalendarPagination.jsx
@@ -1,29 +1,29 @@
 import { SlArrowLeft, SlArrowRight } from 'react-icons/sl';
 import css from './CalendarPagination.module.css';
-import {useState } from 'react';
+import { useState } from 'react';
 import { format, setMonth } from 'date-fns';
+import { useTranslation } from 'react-i18next';
 
-export const CalendarPagination = ({initialDate , onSetDate}) => {
+export const CalendarPagination = ({ initialDate, onSetDate }) => {
+  const { t } = useTranslation();
   const [selectedDate, setSelectedDate] = useState(initialDate ?? new Date());
 
-  const formattedDate = format(selectedDate, 'MMMM, yyyy');
+  const month = t(`months.${format(selectedDate, 'MMMM')}`);
+  const formattedDate = `${month}, ${format(selectedDate, 'yyyy')}`;
 
   const onNextMonth = () => {
     const currentMonth = selectedDate.getMonth();
     const nextDate = setMonth(selectedDate, currentMonth + 1);
     setSelectedDate(nextDate);
-    onSetDate(nextDate)
+    onSetDate(nextDate);
   };
 
   const onPrevMonth = () => {
     const currentMonth = selectedDate.getMonth();
     const prevDate = setMonth(selectedDate, currentMonth - 1);
     setSelectedDate(prevDate);
-    onSetDate(prevDate)
+    onSetDate(prevDate);
   };
-
-  
-
 
   return (
     <div className={css.paginationWrap}>
