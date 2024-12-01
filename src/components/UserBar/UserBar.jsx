ee814dff940bdf2bf49b94b1cabab7998dbc9db6
Eduard <edik.moskalets@gmail.com>
2024-12-01 19:20:22 +0200

    logic

--- src/components/UserBar/UserBar.jsx
+++ src/components/UserBar/UserBar.jsx
@@ -1,12 +1,16 @@
 import css from './UserBar.module.css';
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { TbSettings } from 'react-icons/tb';
 import { MdLogout } from 'react-icons/md';
+import { LuUserCircle2 } from "react-icons/lu";
 import { BsChevronUp, BsChevronDown } from 'react-icons/bs';
 import { useTranslation } from 'react-i18next';
+// import { useDispatch, useSelector } from "react-redux";
+// import { selectUser } from '../../redux/user/selector';
 
 
-// import { LogOutModal } from '../LogOutModal/LogOutModal.jsx';
+
+// import  LogOutModal  from '../LogOutModal/LogOutModal.jsx';
 import { UserSettingsModal } from 'components/UserSettingsModal/UserSettingsModal.jsx';
 import ModalBackdrop from 'components/ModalBackdrop/ModalBackdrop';
 
@@ -17,6 +21,8 @@ const UserBar = ({ name, avatarUrl }) => {
   const buttonRef = useRef(null);
   const [isSettingModalOpen, setSettingModalOpen] = useState(false);
   // const [isLogOutModalOpen, setLogOutModalClose] = useState(false);
+  // const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
 
   const togglePopover = () => {
     setShowPopoverOpen(!showPopoverOpen);
@@ -40,29 +46,57 @@ const UserBar = ({ name, avatarUrl }) => {
   //   isLogOutModalOpen(false);
   // }
 
+  // useEffect(() => {
+  //   if (isLoggedIn) {
+  //     dispatch(fetchUser());
+  //   }
+  // }, [dispatch, isLoggedIn]);
+
+  // const getInitials = (name, email) => {
+  //   if (name) {
+  //     return name.slice(0, 5).toUpperCase();
+  //   } else if (email) {
+  //     return email.slice(0, 5).toUpperCase();
+  //   }
+  //   return "";
+  // };
+
+  // const initials = getInitials(user?.name, user?.email);
+
   return (
     <div className={css.user_button_container}>
-      <button
+      {isLoggedIn ? (<button
         className={css.user_button}
         onClick={togglePopover}
         ref={buttonRef}
       >
-        <span className={css.username}>{name}</span>
-        <img src={avatarUrl} alt="#" className={css.avatar} />
+        <span className={css.username}>{/*{user?.name || user?.email}*/}</span>
+        <img
+          src={avatarUrl}
+          alt="Photo"
+          className={css.avatar} />
+          
+        
+        {/* Popover */}
         {showPopoverOpen ? (
           <BsChevronDown className={css.iconArrow} />
         ) : (
           <BsChevronUp className={css.iconArrow} />
         )}
-      </button>
+      </button>) : (
+          <div>
+            <LuUserCircle2 className={ css.avatarIcon } />
+          </div>
+      )}
+      
 
       {/* Всплывающее окно (popover) */}
       {showPopoverOpen && (
         <div className={css.popover}>
           <button
             type="button"
-              className={css.inPopover}
-              onClick={onSettingModalOpen}
+            className={css.inPopover}
+            onClick={onSettingModalOpen}
           >
             <div className={css.icon_setting}>
               <TbSettings />
@@ -73,9 +107,9 @@ const UserBar = ({ name, avatarUrl }) => {
           </button>
 
           <button
-              type="button"
-              className={css.inPopoverLogOut}
-              // onClick={isLogOutModalOpen}
+            type="button"
+            className={css.inPopoverLogOut}
+          // onClick={isLogOutModalOpen}
           >
             <div className={css.icon_logout}>
               <MdLogout />
@@ -87,27 +121,28 @@ const UserBar = ({ name, avatarUrl }) => {
         </div>
       )}
 
+
       {/* Модальное окно настроек */}
-      {isSettingModalOpen && 
+      {isSettingModalOpen &&
         (<ModalBackdrop>
           <UserSettingsModal
-          onSettingModalClose={onSettingModalClose}
-        />
+            onSettingModalClose={onSettingModalClose}
+          />
         </ModalBackdrop>
-       )}
+        )}
 
-       {/* Модальное окно выхода */}
+      {/* Модальное окно выхода */}
       {/* {isLogOutModalOpen &&
         (<ModalBackdrop>
-        <LogOutModal
-          onClose={handleLogOutModalClose}
-        />
-      </ModalBackdrop>
-      )} */}
+          <LogOutModal
+            isOpen={true}
+            onClose={handleLogOutModalClose}
+          />
+        </ModalBackdrop>
+        )} */}
       
     </div>
   );
-};
-
+}
 
 export default UserBar;
